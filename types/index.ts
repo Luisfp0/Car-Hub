d5d5dbb107f8d301d6507c18ab462f014f612335
Luisfp0 <luis.oliveirabr1@gmail.com>
2024-01-02 18:51:15 -0300

    Feat: SearchManufacturer component

--- types/index.ts
+++ types/index.ts
@@ -1,8 +1,13 @@
 import { MouseEventHandler } from "react";
 
 export interface CustomButtonProps {
-    title: string,
-    containerStyles?: string,
-    handleClick?: MouseEventHandler<HTMLButtonElement>;
-    btnType?: 'button' | 'submit';
+  title: string;
+  containerStyles?: string;
+  handleClick?: MouseEventHandler<HTMLButtonElement>;
+  btnType?: "button" | "submit";
+}
+
+export interface SearchManufactureProps {
+  manufacturer: string;
+  setManufacturer: (manufacture: string) => void;
 }
